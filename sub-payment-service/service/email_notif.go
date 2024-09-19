e0e94ccdfaa6885d831f30048565b4f99afbdddd
razzzp <m.razifp.pramuda@gmail.com>
2024-09-19 19:58:30 +0700

    skeleton

---
+++ sub-payment-service/service/email_notif.go
@@ -0,0 +1,14 @@
+package service
+
+type EmailNotifService interface {
+	NotifyPaymentSucccess()
+}
+
+func NewEmailNotifService() EmailNotifService {
+	return &emailNotifService{}
+}
+
+type emailNotifService struct {
+}
+
+func (es *emailNotifService) NotifyPaymentSucccess()
