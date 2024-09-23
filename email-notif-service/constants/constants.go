87dfddd71d5c4037fee90b1023fef9af318b7eb0
razzzp <m.razifp.pramuda@gmail.com>
2024-09-23 20:29:19 +0700

    refactor mail sending to service

---
+++ email-notif-service/constants/constants.go
@@ -0,0 +1,4 @@
+package constants
+
+const ROOT_TEMPLATE_PATH = "email-templates"
+const SENDER_EMAIL = "no-reply@demomailtrap.com"
--- email-notif-service/server/email-notif.go
+++ email-notif-service/server/email-notif.go
@@ -2,14 +2,9 @@ package server
 
 import (
 	"context"
+	"email-notif-service/constants"
 	"email-notif-service/pb"
 	"email-notif-service/service"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"strings"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,52 +13,53 @@ import (
 func NewEmailNotifServer(
 	paymentService service.SubsPaymentService,
 	userService service.UserService,
+	emailService service.EmailService,
 ) *EmailNotifServer {
 	return &EmailNotifServer{
 		paymentService: paymentService,
 		userService:    userService,
+		emailService:   emailService,
 	}
 }
 
 type EmailNotifServer struct {
 	paymentService service.SubsPaymentService
 	userService    service.UserService
+	emailService   service.EmailService
 	pb.UnimplementedEmailNotifServiceServer
 }
 
 func (es *EmailNotifServer) NotifyPaymentComplete(c context.Context, req *pb.NotifyPaymentCompleteReq) (*pb.NotifyPaymentCompleteResp, error) {
-	// TODO
-	log.Printf("received notify payment complete request")
-	url := "https://sandbox.api.mailtrap.io/api/send/3155742"
-	method := "POST"
-	payload := strings.NewReader(`{"from":{"email":"hello@example.com","name":"Mailtrap Test"},"to":[{"email":"razif.dev@gmail.com"}],"subject":"You are awesome!","text":"Congrats for sending test email with Mailtrap!","category":"Integration Test"}`)
-
-	client := &http.Client{}
-	httpreq, err := http.NewRequest(method, url, payload)
+	// get payment
+	payment, err := es.paymentService.GetPaymentByID(int(req.PaymentId))
 	if err != nil {
-		fmt.Println(err)
-		return nil, status.Errorf(codes.Internal, "error calling mail api: %s", err.Error())
+		return nil, status.Errorf(codes.NotFound, "failed to get payment: %s", err.Error())
 	}
-	httpreq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("MAILTRAP_TOKEN")))
-	httpreq.Header.Add("Content-Type", "application/json")
-
-	res, err := client.Do(httpreq)
-
+	// get user
+	user, err := es.userService.GetUserByID(int(payment.UserId))
 	if err != nil {
-		fmt.Println(err)
-		return nil, status.Errorf(codes.Internal, "error calling mail api: %s", err.Error())
-	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return nil, status.Errorf(codes.Internal, "error calling mail api: %s", err.Error())
+		return nil, status.Errorf(codes.NotFound, "failed to get user: %s", err.Error())
 	}
 
-	fmt.Println(string(body))
+	// create email req
+	emailReq := service.SendEmailRequest{
+		From: service.Address{Email: constants.SENDER_EMAIL, Name: "Breathe.io"},
+		To: []service.Address{
+			{Email: user.Email, Name: user.Username},
+		},
+		Subject:  "Payment Completed - Breathe.io",
+		Text:     "Congrats for sending test email with Mailtrap!",
+		Html:     "",
+		Category: "Payment",
+	}
+
+	err = es.emailService.SendEmail(&emailReq)
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.NotifyPaymentCompleteResp{
 		Status: "OK",
-		Email:  "razif.dev@gmail.com",
+		Email:  user.Email,
 	}, nil
 }
