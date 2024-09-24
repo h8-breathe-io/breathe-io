d23c5c75fa05cbf408b6dfc0fc492ca9ff0b6a04
razzzp <m.razifp.pramuda@gmail.com>
2024-09-24 20:39:34 +0700

    untrack generated protoc files

--- email-notif-service/genpb.sh
+++
@@ -1,4 +0,0 @@
-protoc subs-payment.proto --proto_path ../subs-payment-service/proto --go_out . --go-grpc_out .
-protoc email-notif.proto --proto_path proto --go_out . --go-grpc_out .
-protoc user.proto --proto_path ../user-service/pb --go_out pb --go-grpc_out pb
-protoc air_quality.proto --proto_path ../air-quality-service/pb --go_out pb --go-grpc_out pb
