4078cb5ce2435cfd8a700636f22d29053c954b74
razzzp <m.razifp.pramuda@gmail.com>
2024-09-25 01:14:57 +0700

    add gateway for air qualities, business facilities

--- api-gateway/genpb.sh
+++
@@ -1,5 +0,0 @@
-protoc subs-payment.proto --proto_path ../proto --go_out . --go-grpc_out .
-protoc email-notif.proto --proto_path ../proto --go_out . --go-grpc_out .
-protoc user.proto --proto_path ../user-service/pb --go_out pb --go-grpc_out pb
-protoc air_quality.proto --proto_path ../air-quality-service/pb --go_out pb --go-grpc_out pb 
-protoc location.proto --proto_path ../air-quality-service/pb --go_out pb --go-grpc_out pb 
