d5bcf0d9afe1deefb624918ab3229338e0b11d01
naufalyunan <naufalyunan45@gmail.com>
2024-09-25 19:18:09 +0700

    make recomendation location function

---
+++ air-quality-service/entity/loc.go
@@ -0,0 +1,7 @@
+package entity
+
+type Loc struct {
+	ID        int     `json:"id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
---
+++ air-quality-service/service/user.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"air-quality-service/entity"
+	pb "air-quality-service/pb/generated"
+	"context"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewUserClient() pb.UserClient {
+	addr := os.Getenv("USER_SERVICE_URL")
+	log.Printf("user service url: %s", addr)
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	client := pb.NewUserClient(conn)
+
+	return client
+}
+
+type UserService interface {
+	GetUserByID(id int) (*entity.User, error)
+}
+
+func NewUserService() UserService {
+	return &userService{
+		userClient: NewUserClient(),
+	}
+}
+
+type userService struct {
+	userClient pb.UserClient
+}
+
+// GetUserByID implements UserService.
+func (u *userService) GetUserByID(id int) (*entity.User, error) {
+
+	res, err := u.userClient.GetUser(context.TODO(), &pb.GetUserRequest{
+		Id: uint64(id),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &entity.User{
+		ID:          id,
+		Username:    res.Username,
+		Email:       res.Email,
+		PhoneNumber: res.Phonenumber,
+		Tier:        res.Tier,
+	}, nil
+}
